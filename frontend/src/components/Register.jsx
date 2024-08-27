3e94e032b93e482822dbb257b43e05d8b96148a4
Sajini Weerasinghe <nssweerasinghe@std.appsc.sab.ac.lk>
2024-08-27 23:32:23 +0530

    update | register page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -6,12 +6,13 @@ import { Box, Button, TextField, Typography } from "@mui/material";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import LoginIcon from "@mui/icons-material/Login";
 import { toast } from "react-toastify";
+import backgroundImage from "../images/back2.jpg"; // Ensure the path is correct
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const { dispatch } = useContext(AppContext) ;
+  const { dispatch } = useContext(AppContext);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -30,30 +31,42 @@ const Register = () => {
       toast.success("Registration successful!");
     } catch (err) {
       const errorMessage =
-        err.response?.data?.errors?.[0]?.msg || "Error during register";
+        err.response?.data?.errors?.[0]?.msg || "Error during registration";
       console.error(err);
       toast.error(errorMessage);
     }
   };
 
   return (
-    <div>
+    <Box
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <form onSubmit={handleSubmit}>
         <Box
-          sx={{ ":hover": { boxShadow: "10px 10px 20px #ccc" } }}
+          sx={{
+            ":hover": { boxShadow: "10px 10px 15px #ccc" },
+          }}
           display="flex"
           flexDirection={"column"}
-          maxWidth={400}
+          maxWidth={600}
           alignItems={"center"}
           justifyContent={"center"}
           margin={"auto"}
-          marginTop={5}
-          marginBottom={5}
-          padding={3}
+          paddingX={10}
+          paddingY={3}
           borderRadius={5}
           boxShadow={"5px 5px 10px #ccc"}
+          backgroundColor="rgba(255, 255, 255, 0.8)"
         >
-          <Typography variant="h2" padding={5} fontWeight="500">
+          <Typography variant="h2" padding={5} fontWeight="500" color={"rgb(97, 62, 10,0.9)"}>
             Register
           </Typography>
           <TextField
@@ -65,6 +78,25 @@ const Register = () => {
             onChange={(e) => setUsername(e.target.value)}
             fullWidth
             required
+            sx={{
+              width: "130%",
+              "& .MuiInputBase-root": {
+                color: "black",
+                fontSize: "1.1rem",
+                height: "60px",
+              },
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "#d7ccc8",
+                },
+                "&:hover fieldset": {
+                  borderColor: "#a1887f",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#795548",
+                },
+              },
+            }}
           />
           <TextField
             margin="normal"
@@ -75,6 +107,25 @@ const Register = () => {
             onChange={(e) => setEmail(e.target.value)}
             fullWidth
             required
+            sx={{
+              width: "130%",
+              "& .MuiInputBase-root": {
+                color: "black",
+                fontSize: "1.1rem",
+                height: "60px",
+              },
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "#d7ccc8",
+                },
+                "&:hover fieldset": {
+                  borderColor: "#a1887f",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#795548",
+                },
+              },
+            }}
           />
           <TextField
             margin="normal"
@@ -85,10 +136,37 @@ const Register = () => {
             onChange={(e) => setPassword(e.target.value)}
             fullWidth
             required
+            sx={{
+              width: "130%",
+              marginBottom: 2,
+              "& .MuiInputBase-root": {
+                color: "black",
+                fontSize: "1.1rem",
+                height: "60px",
+              },
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "#d7ccc8",
+                },
+                "&:hover fieldset": {
+                  borderColor: "#a1887f",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#795548",
+                },
+              },
+            }}
           />
           <Button
             startIcon={<PersonAddIcon />}
-            sx={{ marginTop: 3, borderRadius: 3 }}
+            sx={{
+              marginTop: 3,
+              borderRadius: 3,
+              backgroundColor: "#613e0a",
+              ":hover": {
+                backgroundColor: "#3b2402",
+              },
+            }}
             variant="contained"
             color="primary"
             type="submit"
@@ -98,7 +176,14 @@ const Register = () => {
           <Link to="/login">
             <Button
               endIcon={<LoginIcon />}
-              sx={{ marginTop: 6, borderRadius: 3 }}
+              sx={{
+                marginTop: 6,
+                borderRadius: 3,
+                color: "#613e0a",
+                ":hover": {
+                  color: "#3b2402",
+                },
+              }}
               color="secondary"
             >
               Change to Login
@@ -106,7 +191,7 @@ const Register = () => {
           </Link>
         </Box>
       </form>
-    </div>
+    </Box>
   );
 };
 
